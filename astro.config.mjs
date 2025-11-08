3014bcc3c0d82c2ae9bf9a50ddd841923f1b6529
Bryce Kowalczyk <xbc@xtrabyte.com>
2025-11-08 02:15:18 -0500

    Initial website content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
+  site: 'https://xtrabyte-cybersecurity.vercel.app'
+});
